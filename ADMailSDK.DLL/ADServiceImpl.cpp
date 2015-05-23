e51ab5cf9bd69bddbf54e0a7ea93d1b4b6253de2
Дронов Вячеслав <dronovviacheslav@outlook.com>
2015-05-23 15:47:40 +0300

    Push

--- ADMailSDK.DLL/ADServiceImpl.cpp
+++ ADMailSDK.DLL/ADServiceImpl.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "ADService.h"
+#include <windows.applicationmodel.core.h>
+#include <d3
 
 using namespace Microsoft::WRL;
 
@@ -9,6 +11,12 @@ namespace ABI
 	{
 		namespace DLL
 		{
+			using namespace Microsoft::WRL;
+			using namespace Microsoft::WRL::Wrappers;
+			using namespace Windows::Foundation;
+			using namespace Windows::ApplicationModel::Core;
+			using namespace ABI::Windows::UI::Core;
+
 			class ADService : public RuntimeClass<IADService>
 			{
 				InspectableClass(L"ADMailSDK.DLL.ADService", BaseTrust)
@@ -21,6 +29,23 @@ namespace ABI
 				STDMETHODIMP GetInt(INT32* result) override
 				{
 					*result = 10;
+					return S_OK;
+				}
+
+				STDMETHODIMP ShowSomeText() override
+				{
+					HRESULT hr;
+					
+					ComPtr<ICoreApplication> coreApplication;
+					hr = GetActivationFactory(HStringReference(RuntimeClass_Windows_ApplicationModel_Core_CoreApplication).Get(), coreApplication.GetAddressOf());
+
+					ComPtr<ICoreApplicationView> coreApplicationView;
+					hr = coreApplication->GetCurrentView(coreApplicationView.GetAddressOf());
+
+					ComPtr<ICoreWindow> coreWindow;
+					hr = coreApplicationView->get_CoreWindow(coreWindow.GetAddressOf());
+
+
 					return S_OK;
 				}
 			};
--- SilverlightApp/MainPage.xaml.cs
+++ SilverlightApp/MainPage.xaml.cs
@@ -19,28 +19,12 @@ namespace SilverlightApp
         {
             InitializeComponent();
 
-            var service = new ADService();
-            var i = service.GetInt();
-            i++;
+            Loaded += (o, e) =>
+                {
+                    var service = new ADService();
+                    service.ShowSomeText();
+                };
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
-
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
